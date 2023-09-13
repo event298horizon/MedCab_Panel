c4a96237f2d249e5e3598475a78754505a83963d
Saurabh <saurabhawasthi618@gmail.com>
2023-09-13 18:14:33 +0530

    set up complete

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{ url('css/reset.css') }}?<?php echo time(); ?>">
     <link rel="stylesheet" href="{{ url('css/nav.css') }}?<?php echo time(); ?>">
     <link rel="stylesheet" href="{{ url('css/sidebar.css') }}?<?php echo time(); ?>">
+    <link rel="stylesheet" href="{{ url('css/pathology.css') }}?<?php echo time(); ?>">
     <!-- custom css -->
 
     <title>MEDCAB PANEL</title>
@@ -53,108 +54,10 @@
     </nav>
     <!-- NavBar -->
 
-    <!-- SideBar -->
-    <section id="sidebar">
-        <section class=" sidebar border-top border-4 shadow d-flex flex-column gap-5">
-            <div class="tabs upper-tabs d-flex flex-column gap-2 secondary-heading">
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Dashboard
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Facilities
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Ambulances Onway
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Pathology Tests
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Recommend
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Doctors
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Hospitals
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="tabs lower-tabs d-flex flex-column gap-2 secondary-heading">
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Customize
-                        </p>
-                    </div>
-                </div>
-                <div class="tab d-flex align-items-center">
-                    <div>
-                        <img src="{{asset('assets/images/dummy.png')}}" alt="">
-                    </div>
-                    <div>
-                        <p>
-                            Settings
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </section>
-    <!-- SideBar -->
 
-
+    <!-- main section -->
+    @yield('main')
+    <!-- main section -->
 
 
     <!-- HTML -->
--- resources/views/pathology.blade.php
+++ resources/views/pathology.blade.php
@@ -1,20 +1,31 @@
 @extends('layouts.adminlayout')
 
+
 @section('main')
 
-<section class="pathology">
-    <div class="search-bar">
-        <div class="search">
-            <img src="{{asset('assets/images/icons/search-icon.png')}}" alt="search">
-            <input type="text" placeholder="Search test, packages here">
-        </div>
-    </div>
-    <div class="filter">
+<section class="d-flex">
 
-    </div>
-    <div class="main-content">
-        
-    </div>
+    @include('include.sidebar')
+
+    <section class="pathology">
+        <div class="left-section">
+            <div class="search-bar">
+                <div class="search">
+                    <img src="{{asset('assets/images/icons/search-icon.png')}}" alt="search">
+                    <input type="text" placeholder="Search test, packages here">
+                </div>
+            </div>
+            <div class="filter">
+
+            </div>
+            <div class="main-content">
+
+            </div>
+        </div>
+        <div class="right-section">
+            
+        </div>
+    </section>
 </section>
 
 @endsection
